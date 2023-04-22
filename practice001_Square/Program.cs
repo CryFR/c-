a3394edb17c7879497a7e9aa2fb63f1f00dc05b7
Nik <cryforreal@gmail.com>
2023-04-22 14:49:27 +0300

    practice001

---
+++ practice001_Square/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num * num;
+
+Console.Write("Квадрат числа: ");
+Console.WriteLine(result);
+
+if (num > 0)
+{
+    Console.WriteLine("Было введено положительное число.");
+}
+else if (num == 0)
+{
+    Console.WriteLine("Был введен нуль.");
+
+}
+else
+{
+    Console.WriteLine("Было введено отрицательное число.");
+}
