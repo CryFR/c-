ea6d9b5235ad443ccb3691ab011b17e80cd293fd
Nik <cryforreal@gmail.com>
2023-04-22 15:59:00 +0300

    practice007

---
+++ practice/practice007/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход целое число любой разрядности и на выходе показывает вторую цифру слева этого числа или говорит, что такой цифры нет. Через строку решать нельзя.
+// 456111 -> 5
+// 78 -> 8
+// 9146548 -> 1
+// 3 -> нет
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int secondDigit = 0;
+while (number > 0)
+{
+    if (number < 100)
+    {
+        secondDigit = number % 10;
+        break;
+    }
+    number /= 10;
+}
+
+if (number < 10)
+{
+    Console.WriteLine("Второй цифры слева нет.");
+}
+else
+{
+    Console.WriteLine("Вторая цифра слева: " + secondDigit);
+}
