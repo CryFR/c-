601036618075fa92182925be628b94b92da7cd9c
Nik <cryforreal@gmail.com>
2023-04-29 12:38:54 +0300

    optional1

---
+++ practice/optional1/Program.cs
@@ -0,0 +1,18 @@
+﻿// на входе целое или вещественное число, надо удалить вторую цифру слева этого числа.
+
+// 145 -> 15
+// 1 -> нет
+// 567,123 -> 57,123
+
+Console.Write("Введите число: ");
+string input = Console.ReadLine();
+
+if (input.Length >= 2)
+{
+    string output = input.Remove(1, 1);
+    Console.WriteLine(output);
+}
+else
+{
+    Console.WriteLine("нет");
+}
