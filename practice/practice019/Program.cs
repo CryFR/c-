7c4d7dd70f22bd45717a84ddcc76179bef9405a4
Nik <cryforreal@gmail.com>
2023-05-02 21:29:13 +0300

    practice019

---
+++ practice/practice019/Program.cs
@@ -0,0 +1,31 @@
+﻿bool IsPalindrome(int number)
+{
+    int temp = number;
+    int reverse = 0;
+    while (temp != 0)
+    {
+        int remainder = temp % 10;
+        reverse = reverse * 10 + remainder;
+        temp /= 10;
+    }
+    return number == reverse;
+}
+
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 10000 && number <= 99999)
+{
+    if (IsPalindrome(number))
+    {
+        Console.WriteLine("Число является палиндромом.");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом.");
+    }
+}
+else
+{
+    Console.WriteLine("Введено некорректное число.");
+}
