80272c25eb51d1f05d20193cfd5e2cc608414ce4
Nik <cryforreal@gmail.com>
2023-05-24 14:04:05 +0300

    practice027

---
+++ practice/practice027/Program.cs
@@ -0,0 +1,20 @@
+﻿static int GetDigitSum(int number)
+{
+    int sum = 0;
+
+    while (number != 0)
+    {
+        int digit = number % 10;
+        sum += digit;
+        number /= 10;
+    }
+
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = GetDigitSum(number);
+
+Console.WriteLine("Сумма цифр: " + sum);
